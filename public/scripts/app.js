a13a50a359b510ad60dea5d428765c758e60f349
Alvin Lin <alvinch.lin@gmail.com>
2021-05-04 03:05:50 +0000

    Add ajax to load the story elements into the index page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,30 @@
 $(() => {
+  const createStoryElem = (storyObj) => {
+    let status = 'In progress';
+    if (storyObj.complete) {
+      status = 'Completed';
+    }
+    const $storyElem = $(`
+    <article class="story">
+      <div class="story-header">
+        <h3>${storyObj.name}</h3>
+        <h3>${status}</h3>
+      </div>
+      <div class="story-title">${storyObj.story}</div>
+    </article>
+    `);
+    return $storyElem;
+  };
+
+  // const renderStory = ()
+
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+    url: "/stories",
+  }).done((stories) => {
+    for (story of stories) {
+      const $story = createStoryElem(story);
+      $(".container").append($story);
     }
-  });;
+  });
 });
