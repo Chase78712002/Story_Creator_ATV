eb37a7a2f1b9ea3f3cb1d19282ec4e92b9a3b083
Alvin Lin <alvinch.lin@gmail.com>
2021-05-06 20:09:59 +0000

    INCREASE VARCHAR to 10000 in both stories and contributions table

--- db/schema/03_contributions.sql
+++ db/schema/03_contributions.sql
@@ -5,7 +5,7 @@ CREATE TABLE contributions (
   id SERIAL PRIMARY KEY NOT NULL,
   writer_id INTEGER REFERENCES writers(id) ON DELETE CASCADE,
   story_id INTEGER REFERENCES stories(id) ON DELETE CASCADE,
-  contribution VARCHAR(1000) NOT NULL,
+  contribution VARCHAR(10000) NOT NULL,
   date_created TIMESTAMP DEFAULT NOW(),
   vote INTEGER DEFAULT 0,
   accepted BOOLEAN DEFAULT false
